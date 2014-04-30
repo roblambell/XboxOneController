7be6cc4460543834b81ee546519425d07b771468
Adrien JUND <soultaikerdidu@gmail.com>
2014-04-30 13:21:30 +0200

    Fix Test Pad

--- PadReaderTest/Program.cs
+++ PadReaderTest/Program.cs
@@ -15,7 +15,10 @@ namespace PadReaderTest
             ControllerReader myController = ControllerReader.Instance;
             int x = 0;
             while (x < 5)
+            {
                 Thread.Sleep(1);
+                ++x;
+            }
             myController.CloseController();
         }
     }
